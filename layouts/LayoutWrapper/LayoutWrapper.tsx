6965a894474b087122627ba03b7e943ae0617665
Daria Babakova <kofaka@gmail.com>
2023-01-17 16:03:45 +0200

    Add logo with a link to the home page in Header at layouts/LayoutWrapper component.

--- layouts/LayoutWrapper/LayoutWrapper.tsx
+++ layouts/LayoutWrapper/LayoutWrapper.tsx
@@ -1,7 +1,13 @@
 import { ReactNode } from 'react';
+import Link from 'next/link';
+import NextImage from 'next/image';
 import { ConfigProvider, Layout, theme } from 'antd';
+// Constants
+import { HOME } from 'constants/routes';
 // Layout
 import Head from 'layouts/Head/Head';
+// Styles
+import styles from './LayoutWrapper.module.scss';
 
 const { Header, Footer, Content } = Layout;
 
@@ -17,21 +23,40 @@ const LayoutWrapper = ({
   children,
 }: LayoutWrapperProps) => {
   return (
-    <ConfigProvider
-      theme={{
-        algorithm: theme.darkAlgorithm,
-      }}
-    >
+    <>
       <Head title={title} description={description} />
 
-      <Layout>
-        <Header>Header</Header>
+      <ConfigProvider
+        theme={{
+          algorithm: theme.darkAlgorithm,
+        }}
+      >
+        <Layout>
+          <Header className={styles.header}>
+            <Link
+              href={HOME}
+              title="Go home"
+              className={styles.logoWrapper}
+              prefetch={false}
+              passHref
+            >
+              <NextImage
+                src="/kofa-logo.svg"
+                alt="Kofa logo"
+                blurDataURL="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
+                width={56}
+                height={56}
+                priority
+              />
+            </Link>
+          </Header>
 
-        <Content className="site-layout">{children}</Content>
+          <Content className="site-layout">{children}</Content>
 
-        <Footer>Footer</Footer>
-      </Layout>
-    </ConfigProvider>
+          <Footer>Footer</Footer>
+        </Layout>
+      </ConfigProvider>
+    </>
   );
 };
 
