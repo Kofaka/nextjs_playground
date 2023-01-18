e48e39d462a8ea74f5fcbd258ac24b850c7bf52a
Daria Babakova <kofaka@gmail.com>
2023-01-18 18:16:24 +0200

    Create pages/404 (NotFoundPage) page component.

---
+++ pages/404.tsx
@@ -0,0 +1,31 @@
+import { Result, Button } from 'antd';
+import { useRouter } from 'next/router';
+// Constants
+import { HOME } from 'constants/routes';
+// Layouts
+import LayoutWrapper from 'layouts/LayoutWrapper/LayoutWrapper';
+
+const NotFoundPage = () => {
+  const router = useRouter();
+
+  const handleGoHome = () => {
+    router.push(HOME).then();
+  };
+
+  return (
+    <LayoutWrapper title="Page not found" description="Page not found">
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={
+          <Button type="primary" onClick={handleGoHome}>
+            Back Home
+          </Button>
+        }
+      />
+    </LayoutWrapper>
+  );
+};
+
+export default NotFoundPage;
