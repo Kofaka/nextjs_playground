7b112d82b20deef30b3a99e9eb1efa3eb8fd1395
Daria Babakova <kofaka@gmail.com>
2023-01-20 10:26:03 +0200

    Move the LayoutWrapper at the app very top level (pages/_app.tsx)

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,8 +2,6 @@ import { Result, Button } from 'antd';
 import { useRouter } from 'next/router';
 // Constants
 import { HOME } from 'constants/routes';
-// Layouts
-import LayoutWrapper from 'layouts/LayoutWrapper/LayoutWrapper';
 
 const NotFoundPage = () => {
   const router = useRouter();
@@ -13,22 +11,16 @@ const NotFoundPage = () => {
   };
 
   return (
-    <LayoutWrapper
-      title="Page not found"
-      description="Page not found"
-      withSidebar={false}
-    >
-      <Result
-        status="404"
-        title="404"
-        subTitle="Sorry, the page you visited does not exist."
-        extra={
-          <Button type="primary" onClick={handleGoHome}>
-            Back Home
-          </Button>
-        }
-      />
-    </LayoutWrapper>
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={
+        <Button type="primary" onClick={handleGoHome}>
+          Back Home
+        </Button>
+      }
+    />
   );
 };
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,39 @@
+import { useRouter } from 'next/router';
 import type { AppProps } from 'next/app';
+// Constants
+import { HOME, NEW_PAGE, NOT_FOUND } from 'constants/routes';
 // Styles
 import 'styles/globals.scss';
+// Layouts
+import LayoutWrapper from 'layouts/LayoutWrapper/LayoutWrapper';
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
-  return <Component {...pageProps} />;
+  const { pathname } = useRouter();
+
+  const routesData: Record<string, { title: string; description: string }> = {
+    [HOME]: {
+      title: 'The list of SpaseX ships',
+      description: 'The example of page with paginated list',
+    },
+    [NEW_PAGE]: {
+      title: 'Some dummy new page',
+      description: 'Some dummy description',
+    },
+    [NOT_FOUND]: {
+      title: 'Page not found',
+      description: 'Page not found',
+    },
+  };
+
+  return (
+    <LayoutWrapper
+      title={routesData[pathname].title}
+      description={routesData[pathname].description}
+      withSidebar={pathname !== NOT_FOUND}
+    >
+      <Component {...pageProps} />
+    </LayoutWrapper>
+  );
 };
 
 export default App;
